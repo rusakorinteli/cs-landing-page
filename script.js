bed2b72d0acd96eb91b7d2a13cc2f720ec418c23
rusakorinteli <rusudankorinteli0@gmail.com>
2024-03-17 13:52:57 +0400

    changes

--- script.js
+++ script.js
@@ -21,31 +21,6 @@ function showMoreNews() {
 }
 
 document.getElementById("moreButton").addEventListener("click", showMoreNews);
-
-// fetch(apiUrl)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const newsList = document.getElementById("newsList");
-//     totalNews = data.articles.length;
-//     data.articles.forEach((article, index) => {
-//       const li = document.createElement("li");
-//       li.classList.add("newsItem");
-
-//       const link = document.createElement("a");
-//       link.classList.add("newsLink");
-//       link.href = article.url;
-//       link.textContent = article.title;
-
-//       li.appendChild(link);
-//       newsList.appendChild(li);
-
-//       if (index < 5) {
-//         li.style.display = "block";
-//       }
-//     });
-//   })
-//   .catch((error) => console.error("Error fetching news:", error));
-
 fetch(apiUrl)
   .then((response) => {
     if (!response.ok) {
