03e5b7eafca2c5f71f8aa0ba1b597630784fb09e
rusakorinteli <rusudankorinteli0@gmail.com>
2024-03-20 18:14:07 +0400

    add filter

--- burger.js
+++ burger.js
@@ -1,9 +1,23 @@
 export function initializeMenu() {
   let hamMenu = document.querySelector(".ham-menu");
   let offScreenMenu = document.querySelector(".off-screen-menu");
+  let filterInput = document.getElementById("filterInput");
+  let items = document.querySelectorAll(".ul-items li");
 
   hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
+  });
+
+  filterInput.addEventListener("input", function () {
+    let filterValue = filterInput.value.toLowerCase();
+    items.forEach(function (item) {
+      let text = item.textContent.toLowerCase();
+      if (text.includes(filterValue)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
   });
 }
